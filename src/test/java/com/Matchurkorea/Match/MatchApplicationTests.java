281c52efe47170b2b102218f2c75d1661ba2e383
chaewon <cococode12@gmail.com>
2021-08-22 00:46:05 +0900

    base setting

---
+++ src/test/java/com/Matchurkorea/Match/MatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Matchurkorea.Match;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
